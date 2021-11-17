e3d00111e3adcbe5440c7538b44069b9d6dc5cc0
Sergio Minissale <minissale.sergio@outlook.itm>
2021-11-17 15:26:09 +0100

    sistemato codice

--- src/org/generation/italy/array/CibiPreferiti.java
+++ src/org/generation/italy/array/CibiPreferiti.java
@@ -9,7 +9,7 @@ public class CibiPreferiti {
 		
 		System.out.println("La classifica è composta da: " + lunghezzaArray + " posizioni");
 		System.out.println("Il cibo top, che occupa quindi la prima posizione, è: " + cibiPreferiti[0]);
-		System.out.println("Il cibo preferito ma non troppo, che occupa quindi l'ultima posizione, è: " + cibiPreferiti[9]);
+		System.out.println("Il cibo preferito ma non troppo, che occupa quindi l'ultima posizione, è: " + cibiPreferiti[lunghezzaArray - 1]);
 		
 		
 			
