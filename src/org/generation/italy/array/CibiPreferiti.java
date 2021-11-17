d4bb668bfda17277b8c74dc17ceea1be99ef22cc
Sergio Minissale <minissale.sergio@outlook.itm>
2021-11-17 15:56:29 +0100

    bonus

--- src/org/generation/italy/array/CibiPreferiti.java
+++ src/org/generation/italy/array/CibiPreferiti.java
@@ -5,15 +5,24 @@ public class CibiPreferiti {
 	public static void main(String[] args) {
 		
 		String[] cibiPreferiti = {"Sushi", "Pizza", "Carbonara", "Arancini", "Gricia", "Cheesecake", "Cannolo siciliano", "Hamburger", "Cacio e pepe", "Fritto misto"};
+		
 		int lunghezzaArray = cibiPreferiti.length;
 		
 		System.out.println("La classifica è composta da: " + lunghezzaArray + " posizioni");
 		System.out.println("Il cibo top, che occupa quindi la prima posizione, è: " + cibiPreferiti[0]);
 		System.out.println("Il cibo preferito ma non troppo, che occupa quindi l'ultima posizione, è: " + cibiPreferiti[lunghezzaArray - 1]);
 		
-		
+		int resto = lunghezzaArray % 2;
+		int posizioneMediana = lunghezzaArray/2;
+		if(resto != 0) {
 			
-
+			System.out.println("Il cibo che occupa la posizione mediana è: " + cibiPreferiti[posizioneMediana] );
+			
+		} else {
+			
+			System.out.println("Il cibo che occupa la posizione mediana è: " + cibiPreferiti[posizioneMediana + 1] + " e " + cibiPreferiti[posizioneMediana - 1]);
+			//in questo caso la mediana vera e propria non esiste e quindi ho preso in considerazione 2 valori
+		}
 	}
 
 }
