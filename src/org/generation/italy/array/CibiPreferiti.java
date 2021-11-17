3371fde9871e11c95548dc75fb705743e5d7c916
Sergio Minissale <minissale.sergio@outlook.itm>
2021-11-17 15:03:27 +0100

    esercizio finito

---
+++ src/org/generation/italy/array/CibiPreferiti.java
@@ -0,0 +1,19 @@
+package org.generation.italy.array;
+
+public class CibiPreferiti {
+
+	public static void main(String[] args) {
+		
+		String[] cibiPreferiti = {"Sushi", "Pizza", "Carbonara", "Arancini", "Gricia", "Cheesecake", "Cannolo siciliano", "Hamburger", "Cacio e pepe", "Fritto misto"};
+		int lunghezzaArray = cibiPreferiti.length;
+		
+		System.out.println("La classifica è composta da: " + lunghezzaArray + " posizioni");
+		System.out.println("Il cibo top, che occupa quindi la prima posizione, è: " + cibiPreferiti[0]);
+		System.out.println("Il cibo preferito ma non troppo, che occupa quindi l'ultima posizione, è: " + cibiPreferiti[9]);
+		
+		
+			
+
+	}
+
+}
